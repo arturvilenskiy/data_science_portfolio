d6ef120ef025f8fb82453472251476b4ef23a139
Artur Vilenskiy <arturvilenskiy@gmail.com>
2021-11-19 19:39:11 +0000

    Updated pinch, added destination + percentage

--- drag_and_drop_game/drag_drop_game.py
+++ drag_and_drop_game/drag_drop_game.py
@@ -1,24 +1,16 @@
 import cv2
+import random
 from cvzone.HandTrackingModule import HandDetector
 
 def pinch(lmList):
     thumb_tip = lmList[4]
     index_tip = lmList[8]
-    middle_tip = lmList[12]
 
-    if thumb_tip[0] - 60 > index_tip[0]:
-        print('1')
-        return False
-    if thumb_tip[0] + 70 < middle_tip[0]:
-        print('2')
-        return False
-    if thumb_tip[1] - 60 > index_tip[1]:
-        print('3')
-        return False
-    if thumb_tip[1] - 70 > middle_tip[1]:
-        print('4')
-        return False
-    return True
+    if thumb_tip[0] in range(index_tip[0]-10, index_tip[0]+10):
+        return True
+    if thumb_tip[1] in range(index_tip[1]-20, index_tip[1]+20):
+        return True
+    return False
 
 
 def within_bounds(lmlist, location):
@@ -41,20 +33,52 @@ def move_figure(frame, lmlist, location):
     pinch_loc = (lmlist[4][0], lmlist[4][1])
     cv2.circle(frame, pinch_loc, 10, (0, 0, 255), -1)
     cv2.rectangle(frame, location[0], location[1], (255, 0, 255), -1)
-    
 
+def intersection_percentage(figure_location, destination_location):
+    destination_area = (destination_location[1][0] - destination_location[0][0]) * (destination_location[1][1] - destination_location[0][1])
+
+    if figure_location[0][0] in range(destination_location[0][0], destination_location[1][0]) and figure_location[0][1] in range(destination_location[0][1], destination_location[1][1]):
+        occupied_area = (destination_location[1][0] - figure_location[0][0]) * (destination_location[1][1] - figure_location[0][1])
+
+    elif figure_location[1][0] in range(destination_location[0][0], destination_location[1][0]) and figure_location[0][1] in range(destination_location[0][1], destination_location[1][1]):
+        occupied_area = (figure_location[1][0] - destination_location[0][0]) * (destination_location[1][1] - figure_location[0][1])
+    
+    elif figure_location[1][0] in range(destination_location[0][0], destination_location[1][0]) and figure_location[1][1] in range(destination_location[0][1], destination_location[1][1]):
+        occupied_area = (figure_location[1][0] - destination_location[0][0]) * (figure_location[1][1] - destination_location[0][1])
+    
+    elif figure_location[0][0] in range(destination_location[0][0], destination_location[1][0]) and figure_location[1][1] in range(destination_location[0][1], destination_location[1][1]):
+        occupied_area = (destination_location[1][0] - figure_location[0][0]) * (figure_location[1][1] - destination_location[0][1])
+    else:
+        return 0
+    return occupied_area/destination_area*100
+
+def create_figures():
+    width = 1290
+    height = 720
+    fig_x = random.randint(0, 1090)
+    fig_y = random.randint(0, 520)
+
+    dest_x = random.randint(5, 1090)
+    if dest_x in range(fig_x, fig_x + 200):
+        dest_x = random.randint(5, 1090)
+    dest_y = random.randint(5, 520)
+    if dest_y in range(fig_y, fig_y + 200):
+        dest_y = random.randint(5, 520)
+    return [(fig_x, fig_y), (fig_x + 200, fig_y + 200)], [(dest_x, dest_y), (dest_x + 200, dest_y + 200)]
 
 def main():
     cap = cv2.VideoCapture(0)
-    cap.set(3, 1290)
-    cap.set(4, 720)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1290)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     detector = HandDetector(detectionCon=0.8, maxHands=1)
     moved = False
     pinch_continue = False
-    location = [(400,400), (600, 600)]
+    location, destination = create_figures()
+    percentage = 0
     while True:
         success, frame = cap.read()
         hands = detector.findHands(frame, draw=False)
+        cv2.rectangle(frame, (destination[0][0]-5, destination[0][1] - 5), (destination[1][0] + 5, destination[1][1] + 5), (255, 0, 0), 5)
         if not moved:
             cv2.rectangle(frame, location[0], location[1], (255, 0, 255), -1)
         if hands:
@@ -67,13 +91,14 @@ def main():
                 moved = True
                 location = [(lmList[4][0] - changes[0][0], lmList[4][1] - changes[0][1]), (lmList[4][0] + changes[1][0], lmList[4][1] + changes[1][1])]
                 move_figure(frame, lmList, location)
-                """ pinch_loc = (lmList[4][0], lmList[4][1])
-                cv2.circle(frame, pinch_loc, 10, (0, 0, 255), -1) """
+                percentage = intersection_percentage(location, destination)
+                cv2.putText(frame, 'Percentage: ' + str(intersection_percentage(location, destination)) + '%', (50, 50),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
             else:
                 moved = False
                 pinch_continue = False
         else:
             moved = False
+        cv2.putText(frame, 'Percentage: ' + str(percentage) + '%', (50, 50),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
